e52f30d5d294944ecf80e31fdf5d13706cd9f62d
Victor Chinnappan <vchinn04@gmail.com>
2023-04-23 23:29:11 -0700

    Added: __add, getmetatable, and __call functionalities

--- MetaPy.py
+++ MetaPy.py
@@ -9,10 +9,24 @@ class Metatable:
     def __getitem__(self, dict_key):
         if (dict_key not in self.dict):
             if ("__index" in self.meta):
-                self.meta["__index"](self.dict, dict_key)
+                return self.meta["__index"](self.dict, dict_key)
+            else:
+                return None
+        else:
+            return self.dict[dict_key]
 
     def setitem(self, dict_key, value):
         self.dict[dict_key] = value
         if ("__newindex" in self.meta):
             self.meta["__newindex"](self.dict, dict_key, value)
-    
+            
+    def getmetatable(self):
+        return self.meta
+
+    def __add__(self, obj):
+        if ("__add" in self.meta):
+            self.meta["__add"](self.dict, obj)
+
+    def __call__(self, **kwargs):
+        if ("__call" in self.meta):
+            self.meta["__call"](self.dict, **kwargs)
